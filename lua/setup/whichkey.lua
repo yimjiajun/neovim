fdb765fae96455b95653c9d2d295443bb79a3805
Richard Yim <richardyim@ami.com>
2022-09-04 17:30:30 +0800

    lua/setup/whichkey.lua: update whichkey setup
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- lua/setup/whichkey.lua
+++ lua/setup/whichkey.lua
@@ -1,5 +1,4 @@
  require("which-key").setup {
-	{
 		plugins = {
 			marks = true, -- shows a list of your marks on ' and `
 			registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
@@ -31,7 +30,7 @@
 		},
 		icons = {
 			breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-			separator = "➜", -- symbol used between a key and it's label
+			separator = ":", -- symbol used between a key and it's label
 			group = "+", -- symbol prepended to a group
 		},
 		popup_mappings = {
@@ -39,7 +38,7 @@
 			scroll_up = '<c-u>', -- binding to scroll up inside the popup
 		},
 		window = {
-			border = "none", -- none, single, double, shadow
+			border = "single", -- none, single, double, shadow
 			position = "bottom", -- bottom, top
 			margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
 			padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
@@ -48,8 +47,8 @@
 		layout = {
 			height = { min = 4, max = 25 }, -- min and max height of the columns
 			width = { min = 20, max = 50 }, -- min and max width of the columns
-			spacing = 3, -- spacing between columns
-			align = "left", -- align columns left, center or right
+			spacing = 25, -- spacing between columns
+			align = "center", -- align columns left, center or right
 		},
 		ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
 		hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
@@ -63,7 +62,6 @@
 			i = { "j", "k" },
 			v = { "j", "k" },
 		},
-	}
 }
 
 local wk = require("which-key")
