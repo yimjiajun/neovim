9270d1c4f27f97b72e7babcbfea86650e69f9642
Richard Yim <richardyim@ami.com>
2022-09-06 02:42:19 +0800

    [Update]KeyMapping: Bufferline & move action in insert mode
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- lua/config/keymapping.lua
+++ lua/config/keymapping.lua
@@ -3,8 +3,8 @@
 vim.api.nvim_set_keymap('n', '<S-h>', '<C-w>h', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<S-l>', '<C-w>l', { noremap = true, silent = true})
 -- buffer
-vim.api.nvim_set_keymap('n', '<S-Tab>', '<cmd>bprevious<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Tab>', '<cmd>bNext<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<S-Tab>', '<cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Tab>', '<cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<S-c>', '<cmd>bdelete<CR>', { noremap = true, silent = true})
 -- file
 vim.api.nvim_set_keymap('n', '<C-s>', '<cmd>w<CR>', { noremap = true, silent = true})
@@ -13,6 +13,7 @@ vim.api.nvim_set_keymap('n', '<C-s>', '<cmd>w<CR>', { noremap = true, silent = t
 vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true, silent = true})
 -- TERMINAL
 vim.api.nvim_set_keymap('t', 'jk', '<C-\\><C-n>', { noremap = true, silent = true})
