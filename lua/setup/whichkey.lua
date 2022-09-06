bc3209e15e9410751ebb26698ebe3224317c39ec
Richard Yim <richardyim@ami.com>
2022-09-06 14:05:26 +0800

    (Remove)Whichkey: descriptions
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- lua/setup/whichkey.lua
+++ lua/setup/whichkey.lua
@@ -86,14 +86,6 @@ local Terminal  = require('toggleterm.terminal').Terminal
 		end
 
 local wk = require("which-key")
--- As an example, we will create the following mappings:
---  * <leader>ff find files
---  * <leader>fr show recent files
---  * <leader>fb Foobar
--- we'll document:
---  * <leader>fn new file
---  * <leader>fe edit file
--- and hide <leader>1
 
 wk.register({
 	f = {
