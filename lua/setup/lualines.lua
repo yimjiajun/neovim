1c852ad48952c6cbe27f1deed5280b5eb74c65db
Richard Yim <richardyim@ami.com>
2022-09-06 13:27:29 +0800

    (Update)Statusline: hide status line in packer
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- lua/setup/lualines.lua
+++ lua/setup/lualines.lua
@@ -5,7 +5,7 @@ require('lualine').setup {
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
-      statusline = {'NvimTree'},
+      statusline = {'NvimTree', 'packer'},
       winbar = {},
     },
     ignore_focus = {},
