c32781ae89d82dec060c803ee943f25fb37525f8
Richard Yim <richardyim@ami.com>
2022-09-08 14:59:42 +0800

    :(Mod) Keymapping: tempory exit keys in termial mode
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- lua/config/keymapping.lua
+++ lua/config/keymapping.lua
@@ -16,5 +16,5 @@ vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', 'lkj', '<ESC>', { noremap = true, silent = true})
 -- TERMINAL
-vim.api.nvim_set_keymap('t', 'jk', '<C-\\><C-n>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', 'lkj', '<C-\\><C-n>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('t', '\\][', '<C-c> exit<CR>', { noremap = true, silent = true})
