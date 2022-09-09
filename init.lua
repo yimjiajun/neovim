ac669c75b352bb48671f6d0dbfb900eb77bd9c56
Richard Yim <richardyim@ami.com>
2022-09-10 00:38:44 +0800

    (Update) init.lua: config lua files required by lua programming
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
-require('config.settings')
-require('config.autocmd')
-require('config.plugins')
-require('config.keymapping')
-require('config.colors')
-require('config.custom')
+for _, source in ipairs {
+	"config.settings",
+	"config.autocmd",
+	"config.plugins",
+	"config.keymapping",
+	"config.colors",
+	"config.custom",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
