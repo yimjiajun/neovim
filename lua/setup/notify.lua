4f132ef182d09f5a99984fbc774cb67c583ca2af
Richard Yim <richardyim@ami.com>
2022-09-10 03:53:33 +0800

    (Add) Notify: Suppress error message from lagn servers
    'Client 1 quit with exit code 1 and signal 0' will notified when first time enter insert mode
    
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- lua/setup/notify.lua
+++ lua/setup/notify.lua
@@ -18,3 +18,15 @@ require('notify').setup({
 
 -- replace vim notify by using notify plugin
 vim.notify = require("notify")
+
+-- suppress error messages from lang servers
+vim.notify = function(msg, log_level, _opts)
+    if msg:match("exit code") then
+        return
+    end
+    if log_level == vim.log.levels.ERROR then
+        vim.api.nvim_err_writeln(msg)
+    else
+        vim.api.nvim_echo({{msg}}, true, {})
+    end
+end
