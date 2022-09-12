f21abe9bf08730375650a865a5bc1e42f0925d0f
Richard Yim <richardyim@ami.com>
2022-09-12 09:44:23 +0800

    (Add) lualines: no display status line in alpha dashboard
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- lua/setup/lualines.lua
+++ lua/setup/lualines.lua
@@ -5,7 +5,7 @@ require('lualine').setup {
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
-      statusline = {'NvimTree', 'packer'},
+      statusline = {'NvimTree', 'packer', 'alpha'},
       winbar = {},
     },
     ignore_focus = {},
