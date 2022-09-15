f158496adf90729475e2f0929b141ea1041af667
Richard Yim <richardyim@ami.com>
2022-09-15 16:13:33 +0800

    (Mod) lualine: disable aerial and component seperator
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- lua/setup/lualines.lua
+++ lua/setup/lualines.lua
@@ -2,10 +2,10 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
-      statusline = {'NvimTree', 'packer', 'alpha'},
+      statusline = {'NvimTree', 'packer', 'alpha', 'aerial'},
       winbar = {},
     },
     ignore_focus = {},
