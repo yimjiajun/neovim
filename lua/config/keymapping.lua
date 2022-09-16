a88472a8a377fa2700e6136309b174f516e4f850
Richard Yim <richardyim@ami.com>
2022-09-17 05:09:23 +0800

    (Update) alpha and yank keymapping
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- lua/config/keymapping.lua
+++ lua/config/keymapping.lua
@@ -7,16 +7,15 @@ vim.api.nvim_set_keymap('n', '<CR>', '<cmd> set hlsearch! <CR>', { noremap = tru
 vim.api.nvim_set_keymap('n', '<S-Tab>', '<cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Tab>', '<cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<C-c>', '<cmd>bdelete<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'x', '\"_x', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'X', '\"_X', { noremap = true, silent = true})
 -- file
 vim.api.nvim_set_keymap('n', '<C-s>', '<cmd>w<CR>', { noremap = true, silent = true})
 -- INSERT
 -- move
--- vim.api.nvim_set_keymap('i', '<C-H>', '<Left>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', '<C-b>', '<Left>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', '<esc>b', '<C-o>b', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true, silent = true})
--- vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<C-d>', '<del>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', '<C-f>', '<Right>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', '<esc>f', '<C-o>w', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', 'lkj', '<ESC>', { noremap = true, silent = true})
@@ -25,3 +24,8 @@ vim.api.nvim_set_keymap('t', 'lkj', '<C-\\><C-n>', { noremap = true, silent = tr
 vim.api.nvim_set_keymap('t', '\\][', '<C-c> exit<CR>', { noremap = true, silent = true})
 -- COMMAND LINE
 vim.api.nvim_set_keymap('c', '<C-l>', '<Right>', { noremap = true, silent = true})
+-- VISUAL
+-- Don't yank on delete char
+vim.api.nvim_set_keymap("v", "x", '\"_x', { noremap = true, silent = true})
+vim.api.nvim_set_keymap("v", "X", '\"_X', { noremap = true, silent = true})
+vim.api.nvim_set_keymap("v", "p", '\"_dP', { noremap = true, silent = true})
