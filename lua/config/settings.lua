8d8a1db4830892c5a02f2fb73fd2f72920199e9d
Richard Yim <richardyim@ami.com>
2022-09-23 21:57:06 +0800

    (add) setting: no redraw when executing macro
    Signed-off-by: Richard Yim <richardyim@ami.com>

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -2,6 +2,7 @@
 
 vim.api.nvim_set_option('hidden', true)
 vim.api.nvim_set_option('clipboard', 'unnamedplus')
+vim.api.nvim_set_option('lazyredraw', true)
 vim.api.nvim_set_option('timeoutlen', 300)
 vim.api.nvim_set_option('updatetime', 400)
 -- view
