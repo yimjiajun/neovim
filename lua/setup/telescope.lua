f0260b7c2df5604d6cea522a8f556f738a33dca1
JiaJun Yim <yimjiajun@icloud.com>
2022-09-25 23:09:20 +0800

    (add) telescope: prompt_prefix logo
    Signed-off-by: JiaJun Yim <yimjiajun@icloud.com>

--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -1,11 +1,12 @@
-  require('telescope').setup{
-    defaults = {
-      layout_strategy = 'horizontal',
-      layout_config = {
-		  height = 0.80,
-		  width = 0.95,
-		  prompt_position = 'bottom',
-		  -- mirror = true,
-	  },
-    },
-  }
+require('telescope').setup{
+	defaults = {
+		layout_strategy = 'horizontal',
+			layout_config = {
+			height = 0.80,
+			width = 0.95,
+			prompt_position = 'bottom',
+			-- mirror = true,
+		},
+	prompt_prefix='  ',
+	},
+}
