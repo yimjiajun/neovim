a43f5bc14d39757f4fb523e13f4fc4dc8b12e63f
JiaJun Yim <yimjiajun@icloud.com>
2022-09-25 23:07:32 +0800

    (mod) custom: custom token moved to custom.lua
    Signed-off-by: JiaJun Yim <yimjiajun@icloud.com>

--- init.lua
+++ init.lua
@@ -1,22 +1,11 @@
-vim.g.custom = {
-	-- support native nvim LSP ( 0: disabled native nvim LSP and replaced by coc.nvim )
-	lsp_support = 1,
-	-- display statusline as simple line
-	lualine_simple = 1,
-	-- display buffer
-	buffer_display = 0,
-	-- statusline support
-	statusline_support = 0,
-}
-
 for _, source in ipairs {
+	"config.custom",
 	"config.plugins",
 	"config.settings",
 	"config.autocmd",
 	"config.keymapping",
 	"config.colors",
 	"config.compiler",
-	"config.custom",
 } do
   local status_ok, fault = pcall(require, source)
   if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
---
+++ lua/config/custom.lua
@@ -0,0 +1,12 @@
+vim.g.custom = {
+	-- support native nvim LSP ( 0: disabled native nvim LSP and replaced by coc.nvim )
+	lsp_support = 1,
+	-- display statusline as simple line
+	lualine_simple = 1,
+	-- display buffer
+	buffer_display = 0,
+	-- statusline support
+	statusline_support = 0,
+	-- theme selection [ tundra, gruvbox ]
+	theme = 'tundra',
+}
