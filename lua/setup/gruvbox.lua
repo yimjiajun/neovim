a69652b7ee7f3d1ba69ab6a7c7104ce2ff47db35
JiaJun Yim <yimjiajun@icloud.com>
2022-10-04 13:46:32 +0800

    (mod) gruvbox: tabline non-invert color
    Signed-off-by: JiaJun Yim <yimjiajun@icloud.com>

--- lua/setup/gruvbox.lua
+++ lua/setup/gruvbox.lua
@@ -8,7 +8,7 @@ require("gruvbox").setup({
   strikethrough = true,
   invert_selection = false,
   invert_signs = false,
-  invert_tabline = true,
+  invert_tabline = false,
   invert_intend_guides = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
   contrast = "hard", -- can be "hard", "soft" or empty string
