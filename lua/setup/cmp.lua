c1b0c6cd46d3081930aa7367b9b1b80ac749ad9f
JiaJun Yim <yimjiajun@icloud.com>
2022-10-18 13:58:59 +0800

    (fix) cmp: nvim cmp lsp function deprecated
    Signed-off-by: JiaJun Yim <yimjiajun@icloud.com>

--- lua/setup/cmp.lua
+++ lua/setup/cmp.lua
@@ -105,8 +105,8 @@ cmp.setup({
   })
 
   -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+	local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
   --
   -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
   --   capabilities = capabilities
