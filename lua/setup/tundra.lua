cbc6a8fe32def84cb39f20b4834b95d2d5e4612e
JiaJun <yimjiajun@icloud.com>
2022-12-08 00:49:59 +0800

    [upd] tundra: support transparent background
    Signed-off-by: JiaJun <yimjiajun@icloud.com>

--- lua/setup/tundra.lua
+++ lua/setup/tundra.lua
@@ -1,5 +1,5 @@
-require('nvim-tundra').setup({
-  transparent_background = false,
+local setup = {
+  transparent_background = true,
   editor = {
     search = {},
     substitute = {},
@@ -41,19 +41,45 @@ require('nvim-tundra').setup({
 			DiffDelete = { bg = '#5f0000', italic = true },
 			DiffAdd = { bg = '#262626', italic = true },
 			DiffChange = { bg = '#121212', italic = true },
-			Normal = { bg = '#000000' },
-			NormalNC = { bg = '#000000' },
+			-- Normal = { bg = '#000000' },
+			-- NormalNC = { bg = '#000000' },
 			Visual = { bg = '#1f2937', fg = '#f9fafb'},
-			TelescopePreviewBorder = { fg = '#f9fafb',  bg = '#000000'},
-			TelescopePromptBorder = { fg = '#f9fafb',  bg = '#000000'},
-			TelescopePromptNormal = { bg = '#000000'},
-			TelescopeResultsBorder = { fg = '#f9fafb', bg = '#000000'},
+			TelescopePreviewBorder = { fg = '#f9fafb',  bg = 'none'},
+			TelescopePreviewNormal = { bg = 'none'},
+			TelescopePromptBorder = { fg = '#f9fafb',  bg = 'none'},
+			TelescopePromptNormal = { bg = 'none'},
+			TelescopeResultsBorder = { fg = '#f9fafb', bg = 'none'},
+			TelescopeResultsNormal = { bg = 'none'},
 			TelescopeResultsTitle = { bg = '#b5e8b0' },
+			NormalFloat = { bg = 'none' },
+			NormalFloatNC = { bg = 'none' },
 		},
   },
-})
+};
+
 
 if vim.g.custom.theme == 'tundra' then
+	if vim.g.custom.transparent_background == 0 then
+		setup = {
+			transparent_background = false,
+			overwrite = {
+				highlights = {
+					Normal = { bg = '#000000' },
+					NormalNC = { bg = '#000000' },
+					TelescopePreviewBorder = { fg = '#f9fafb',  bg = '#000000'},
+					TelescopePreviewNormal = { bg = '#000000'},
+					TelescopePromptBorder = { fg = '#f9fafb',  bg = '#000000'},
+					TelescopePromptNormal = { bg = '#000000'},
+					TelescopeResultsBorder = { fg = '#f9fafb', bg = '#000000'},
+					TelescopeResultsNormal = { bg = '#000000'},
+					TelescopeResultsTitle = { bg = '#b5e8b0' },
+				},
+			},
+		};
+	end
+
+	require('nvim-tundra').setup(setup)
+
 	vim.opt.background = 'dark'
 	vim.cmd[[colorscheme tundra]]
 end
