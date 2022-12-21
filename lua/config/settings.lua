3f8279a44e7a16c064c21f44e0f7b1720850e594
JiaJun Yim <yimjiajun@icloud.com>
2022-12-21 15:12:21 +0800

    [upd] setting: timeout length descrease to 200ms
    the longer time to wait for a mapped sequence to complete will denied the pressed key before timeout
    
    Signed-off-by: JiaJun Yim <yimjiajun@icloud.com>

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_set_option('hidden', true)
 vim.api.nvim_set_option('clipboard', 'unnamedplus')
 vim.api.nvim_set_option('lazyredraw', true)
-vim.api.nvim_set_option('timeoutlen', 500)
+vim.api.nvim_set_option('timeoutlen', 200)
 vim.api.nvim_set_option('updatetime', 400)
 -- view
 vim.api.nvim_set_option('termguicolors', true)
