9754495a05000e8e485ed8d10adb3c1e150a6e12
JiaJun Yim <yimjiajun@icloud.com>
2023-01-12 10:14:43 +0800

    refactor lua/config/setting: laststatus
    Problem:
    status line only display when mutiple windows is display
    
    Root cuase:
    Vim 'set laststatus=1', this will only display when mutiple window appear
    
    Solution:
    vim become 'laststatus=2' which will always display status line like default vim setting.
    
    Signed-off-by: JiaJun Yim <yimjiajun@icloud.com>

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -7,7 +7,7 @@ vim.api.nvim_set_option('updatetime', 400)
 -- view
 vim.api.nvim_set_option('termguicolors', true)
 vim.api.nvim_set_option('showmode', true)
-vim.api.nvim_set_option('laststatus', 1)
+vim.api.nvim_set_option('laststatus', 2)
 if vim.fn.has('nvim-0.8') == 1 then
 	vim.api.nvim_set_option('cmdheight', 1)
 end
