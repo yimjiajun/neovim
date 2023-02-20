10127cc380fdabeabdc7d0a0626773e381f70673
JiaJun <yimjiajun@icloud.com>
2023-02-20 11:51:56 +0800

    fix wildmenu: disabled cmp setup cmdline
    this will disabled wildmenu when pressed tab key, and suggestion wouldn't being display
    
    Signed-off-by: JiaJun <yimjiajun@icloud.com>

--- lua/setup/cmp.lua
+++ lua/setup/cmp.lua
@@ -86,24 +86,6 @@ cmp.setup({
     })
   })
 
-  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  })
-
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
-  })
-
   -- Set up lspconfig.
 	local capabilities = require('cmp_nvim_lsp').default_capabilities()
 	-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
