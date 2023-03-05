f0079a80c128288b1f5739dc2fbef5435d2a7f2b
JiaJun <yimjiajun@icloud.com>
2023-03-06 07:13:27 +0800

    feat autocmd: tab format for python and markdown file

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -91,6 +91,28 @@ vim.api.nvim_create_augroup( "extension file", { clear = true })
 			vim.opt_local.expandtab = false
 		end,
 	})
+	vim.api.nvim_create_autocmd( "FileType", {
+		desc = "python",
+		group = "extension file",
+		pattern = "py",
+		callback = function()
+			vim.opt_local.softtabstop = 4
+			vim.opt_local.tabstop = 4
+			vim.opt_local.shiftwidth = 4
+			vim.opt_local.expandtab = true
+		end,
+	})
+	vim.api.nvim_create_autocmd( "FileType", {
+		desc = "markdown",
+		group = "extension file",
+		pattern = "markdown",
+		callback = function()
+			vim.opt_local.softtabstop = 2
+			vim.opt_local.tabstop = 2
+			vim.opt_local.shiftwidth = 2
+			vim.opt_local.expandtab = true
+		end,
+	})
 	vim.api.nvim_create_autocmd( "BufWritePre", {
 		desc = "kill trailing whitespace",
 		group = "extension file",
