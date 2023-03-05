b6d68ed80572db178b5dbca4c42bbdbfb75b26e0
JiaJun <yimjiajun@icloud.com>
2023-03-06 07:10:26 +0800

    feat compiler: include gcc, python, perl and sh compiler build or make by build-in 'make' command

--- lua/config/compiler.lua
+++ lua/config/compiler.lua
@@ -1,7 +1,51 @@
+local prj_found = nil
+
 if io.open('ecfw-zephyr') then
+	prj_found = true
+
 	if io.open('ecfw-zephyr/build.sh') then
 		vim.api.nvim_set_option('makeprg', [[cd $(west topdir)/$(west config manifest.path) && ./build.sh]])
 	else
 		vim.api.nvim_set_option('makeprg', [[west build -p=always -d $(west config manifest.path)/build $(west config manifest.path)]])
 	end
 end
+
+vim.api.nvim_create_augroup( "compiler", { clear = true })
+if prj_found ~= true then
+	vim.api.nvim_create_autocmd( "FileType", {
+		desc = "GCC",
+		group = "compiler",
+		pattern = "c",
+		callback = function()
+			local bufnr = vim.api.nvim_get_current_buf()
+			vim.api.nvim_buf_set_option(bufnr, 'makeprg', [[gcc % -o %:t:r && mv %:t:r /tmp/%:t:r && /tmp/%:t:r && rm /tmp/%:t:r]])
+		end,
+	})
+end
+	vim.api.nvim_create_autocmd( "FileType", {
+		desc = "python",
+		group = "compiler",
+		pattern = "python",
+		callback = function()
+			local bufnr = vim.api.nvim_get_current_buf()
+			vim.api.nvim_buf_set_option(bufnr, 'makeprg', [[python3 %]])
+		end,
+	})
+	vim.api.nvim_create_autocmd( "FileType", {
+		desc = "perl",
+		group = "compiler",
+		pattern = "perl",
+		callback = function()
+			local bufnr = vim.api.nvim_get_current_buf()
+			vim.api.nvim_buf_set_option(bufnr, 'makeprg', [[perl %]])
+		end,
+	})
+	vim.api.nvim_create_autocmd( "FileType", {
+		desc = "bash script",
+		group = "compiler",
+		pattern = "sh",
+		callback = function()
+			local bufnr = vim.api.nvim_get_current_buf()
+			vim.api.nvim_buf_set_option(bufnr, 'makeprg', [[./%]])
+		end,
+	})
