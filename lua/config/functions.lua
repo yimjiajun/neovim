d23b5cd5efbaf36f9b614073cbd44968025ba150
JiaJun Yim <yimjiajun@icloud.com>
2023-03-22 14:47:05 +0800

    feat config/functions: change working directory, get file directory path, get file name and get directory path with pattern search

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,4 +1,38 @@
-function Chwd()
+local lfs = require("lfs")
+
+local function get_dirs_with_pattern(pattern)
+  for dir in lfs.dir(".") do
+    if dir ~= "." and dir ~= ".." then
+      local dir_path = "./" .. dir
+      local mode = lfs.attributes(dir_path, "mode")
+      if mode == "directory" and dir:match(pattern) then
+				return dir_path
+      end
+    end
+  end
+	return nil
+end
+
+local function get_file_dir()
 	local current_file_dir = vim.fn.expand('%:p:h')
+	return current_file_dir
+end
+
+local function get_file_name()
+	local current_file_name = vim.fn.expand('%:t')
+	return current_file_name
+end
+
+local function do_chg_wd()
+	local current_file_dir = get_file_dir()
 	vim.loop.chdir(current_file_dir)
 end
+
+vim.myfn = {
+	doChgwd = do_chg_wd,
+	getFileDir = get_file_dir,
+	getFileName = get_file_name,
+	getDirWithPattern = get_dirs_with_pattern,
+}
+
+return vim.myfn
