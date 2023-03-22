25a1142d0f7d8480651ff77a87bf83f56b0bb8c9
JiaJun Yim <yimjiajun@icloud.com>
2023-03-22 14:47:56 +0800

    feat config/compiler: set build rule for any character match 'ecfw'*'zephyr'

--- lua/config/compiler.lua
+++ lua/config/compiler.lua
@@ -1,9 +1,16 @@
 local prj_found = nil
+local path = nil
+local func = require("config.functions")
 
-if io.open('ecfw-zephyr') then
+path = func.getDirWithPattern("^ecfw.-zephyr$")
+if path ~= nil  then
+	local build_script = 'build.sh'
+
 	prj_found = true
+	path = path .. '/' .. build_script
+	vim.fs.normalize(path)
 
-	if io.open('ecfw-zephyr/build.sh') then
+	if io.open(path) then
 		vim.api.nvim_set_option('makeprg', [[cd $(west topdir)/$(west config manifest.path) && ./build.sh]])
 	else
 		vim.api.nvim_set_option('makeprg', [[west build -p=always -d $(west config manifest.path)/build $(west config manifest.path)]])
@@ -11,19 +18,21 @@ if io.open('ecfw-zephyr') then
 end
 
 vim.api.nvim_create_augroup( "compiler", { clear = true })
-if prj_found ~= true then
-	vim.api.nvim_create_autocmd( "FileType", {
-		desc = "GCC",
-		group = "compiler",
-		pattern = "c",
-		callback = function()
-		if not io.open(string.lower('Makefile')) then
-				local bufnr = vim.api.nvim_get_current_buf()
-				vim.api.nvim_buf_set_option(bufnr, 'makeprg', [[gcc % -o %:t:r && mv %:t:r /tmp/%:t:r && /tmp/%:t:r && rm /tmp/%:t:r]])
-			end
-		end,
-	})
-end
+
+	if prj_found ~= true then
+		vim.api.nvim_create_autocmd( "FileType", {
+			desc = "GCC",
+			group = "compiler",
+			pattern = "c",
+			callback = function()
+			if not io.open(string.lower('Makefile')) then
+					local bufnr = vim.api.nvim_get_current_buf()
+					vim.api.nvim_buf_set_option(bufnr, 'makeprg', [[gcc % -o %:t:r && mv %:t:r /tmp/%:t:r && /tmp/%:t:r && rm /tmp/%:t:r]])
+				end
+			end,
+		})
+	end
+
 	vim.api.nvim_create_autocmd( "FileType", {
 		desc = "python",
 		group = "compiler",
