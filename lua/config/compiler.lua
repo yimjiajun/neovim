506f766f91a6650c9fd6c90564c29672e657864b
JiaJun Yim <yimjiajun@icloud.com>
2023-04-01 14:11:07 +0800

    feat config/compiler: includes markdown makprg for mdbook and Markdown-preview plugins
    1. mdbook:
       - only book.toml exist
       > build when book directory not exist
       > run from browser by default port <3000> and hostname <localhost>
    
    2. markdown-preview plugin:
      - this should run with vim.dispatch plugin via Tmux

--- lua/config/compiler.lua
+++ lua/config/compiler.lua
@@ -1,21 +1,37 @@
 local prj_found = nil
-local path = nil
 local func = require("config.functions")
 
-path = func.getDirWithPattern("^ecfw.-zephyr$")
-if path ~= nil  then
-	local build_script = 'build.sh'
+local function makeprg_mdbook()
+	local bufnr = vim.api.nvim_get_current_buf()
 
-	prj_found = true
-	path = path .. '/' .. build_script
-	vim.fs.normalize(path)
+	if not io.open(string.lower('book')) then
+		vim.api.nvim_buf_set_option(bufnr, 'makeprg', [[mdbook build]])
+	end
 
-	if io.open(path) then
-		vim.api.nvim_set_option('makeprg', [[cd $(west topdir)/$(west config manifest.path) && ./build.sh]])
-	else
-		vim.api.nvim_set_option('makeprg', [[west build -p=always -d $(west config manifest.path)/build $(west config manifest.path)]])
+	vim.api.nvim_buf_set_option(bufnr, 'makeprg', " if [[ ! -d book ]]; then mdbook build; fi mdbook serve")
+end
+
+local function makeprg_zephyr()
+	local path = nil
+
+	path = func.getDirWithPattern("^ecfw.-zephyr$")
+
+	if path ~= nil  then
+		local build_script = 'build.sh'
+
+		prj_found = true
+		path = path .. '/' .. build_script
+		vim.fs.normalize(path)
+
+		if io.open(path) then
+			vim.api.nvim_set_option('makeprg', [[cd $(west topdir)/$(west config manifest.path) && ./build.sh]])
+		else
+			vim.api.nvim_set_option('makeprg', [[west build -p=always -d $(west config manifest.path)/build $(west config manifest.path)]])
+		end
 	end
 end
+
+makeprg_zephyr()
 
 vim.api.nvim_create_augroup( "compiler", { clear = true })
 
@@ -58,5 +74,19 @@ vim.api.nvim_create_augroup( "compiler", { clear = true })
 		callback = function()
 			local bufnr = vim.api.nvim_get_current_buf()
 			vim.api.nvim_buf_set_option(bufnr, 'makeprg', [[./%]])
+		end,
+	})
+	vim.api.nvim_create_autocmd( "FileType", {
+		desc = "markdown",
+		group = "compiler",
+		pattern = "markdown",
+		callback = function()
+			local bufnr = vim.api.nvim_get_current_buf()
+			if io.open(string.lower('book.toml')) then
+				makeprg_mdbook()
+			elseif vim.fn.exists('g:mkdp_command_for_global') == 1 then
+				-- this only supporting on Make (vim dispatch plugin)
+				vim.api.nvim_buf_set_option(bufnr, 'makeprg', [[nvim +MarkdownPreview %]])
+			end
 		end,
 	})
