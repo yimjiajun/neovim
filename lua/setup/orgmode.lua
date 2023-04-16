c95cb05c8b95eff4d5aa9484956ccd504f35e578
JiaJun <yimjiajun@icloud.com>
2023-04-16 20:53:00 +0800

    feat org-mode: include org files from specific remote path
    remote server: Dropbox

--- lua/setup/orgmode.lua
+++ lua/setup/orgmode.lua
@@ -1,8 +1,26 @@
 local M
 
+local function get_orgmode_remote_path()
+	local path
+	if vim.fn.has('unix') == 1 or vim.fn.has('mac') == 1 then
+		if vim.fn.isdirectory("/run/wsl") == 1 then
+			path = vim.fn.expand("/mnt/c/Users/**/Dropbox")
+		else
+			path = vim.fn.expand("$HOME/Dropbox")
+		end
+	elseif vim.fn.has('win32') == 1 then
+		path = vim.fn.expand("C:\\Users\\**\\Dropbox")
+	end
+	return path
+end
+
+local remote_path = get_orgmode_remote_path()
+local usr_org_file = remote_path .. '/' .. vim.fn.expand("org/refile.org")
+local usr_org_agenda_files = { remote_path .. '/' .. '**/*' }
+
 M = {
-	org_agenda_files = {'~/Public/doc/org/**/*', '~/my-orgs/**/*'},
-	org_default_notes_file = '~/Public/doc/org/refile.org',
+	org_agenda_files = usr_org_agenda_files,
+	org_default_notes_file = usr_org_file,
 }
 
 if pcall(require, "which-key") then
