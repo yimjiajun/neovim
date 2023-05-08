cc2bf7d72a075d53c03474f8e80248a811980147
JiaJun Yim <yimjiajun@icloud.com>
2023-05-09 01:57:07 +0800

    feat autocmd: disable advance neovim features and plugin on larger file

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -88,3 +88,32 @@ augroup Binary
     autocmd BufWritePost *.bin set nomod | endif
 augroup END
 ]])
+
+vim.cmd([[
+	let g:large_file = 500 * 1048" 500KB
+
+	" Set options:
+	"   eventignore+=FileType (no syntax highlighting etc
+	"   assumes FileType always on)
+	"   noswapfile (save copy of file)
+	"   bufhidden=unload (save memory when other file is viewed)
+	"   buftype=nowritefile (is read-only)
+	"   undolevels=-1 (no undo possible)
+	au BufReadPre *
+		\ let f=expand("<afile>") |
+		\ if getfsize(f) > g:large_file |
+		\ set eventignore+=FileType |
+		\ setlocal noswapfile bufhidden=unload buftype=nowrite undolevels=-1 |
+		\ if getfsize(f) <= (1048*1048*1048) |
+		\ if getfsize(f) < (1048*1048) |
+		\ echo "large file :" (getfsize(f)/1048)"KB" |
+		\ else |
+		\ echo "large file :" (getfsize(f)/1048/1048)(getfsize(f)/1048%1000)"MB" |
+		\ endif |
+		\ else |
+		\ echo "large file :" (getfsize(f)/1048/1048/1048)(getfsize(f)/1048/1048%1000)"GB" |
+		\ endif |
+		\ else |
+		\ set eventignore-=FileType |
+		\ endif
+]])
