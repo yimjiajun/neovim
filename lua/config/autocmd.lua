63677bfbbd81a90af09e9ae0aaf6fd2e73f8df1b
JiaJun Yim <yimjiajun@icloud.com>
2023-05-09 01:43:06 +0800

    feat autocmd: review bin files

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -27,3 +27,16 @@ vim.api.nvim_create_autocmd( "BufWritePre", {
 		vim.cmd([[%s/\r\+$//e]])
 	end,
 })
+
+vim.cmd([[
+augroup Binary
+    autocmd!
+    autocmd BufReadPre *.bin let &bin=1
+    autocmd BufReadPost *.bin if &bin | %!xxd
+    autocmd BufReadPost *.bin set ft=xxd | endif
+    autocmd BufWritePre *.bin if &bin | %!xxd -r
+    autocmd BufWritePre *.bin endif
+    autocmd BufWritePost *.bin if &bin | %!xxd
+    autocmd BufWritePost *.bin set nomod | endif
+augroup END
+]])
