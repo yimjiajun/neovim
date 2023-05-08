c22ec229a4b1d3c13b4c5b2d6137f0d2e7603fe8
JiaJun Yim <yimjiajun@icloud.com>
2023-05-08 23:39:25 +0800

    feat autocmd: killing whitespace trailing

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -2,8 +2,19 @@ vim.api.nvim_create_augroup( "session", { clear = true })
 	vim.api.nvim_create_autocmd( "VimLeavePre", {
 		desc = "Save Session Before Leave Neovim",
 		group = "session",
-		pattern = "*",
+		pattern = "*.*",
 		callback = function()
 			require("config.function").Session("save")
 		end,
 	})
+
+vim.api.nvim_create_augroup( "format", { clear = true })
+
+vim.api.nvim_create_autocmd( "BufWritePre", {
+	desc = "Killing whitespace trailing",
+	group = "format",
+	pattern = "*",
+	callback = function()
+		vim.cmd([[%s/\s\+$//e]])
+	end,
+})
