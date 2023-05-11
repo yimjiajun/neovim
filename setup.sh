ea99f683bd5be99d9f404651068efa7a79b77619
JiaJun Yim <yimjiajun@icloud.com>
2023-05-11 11:03:17 -0700

    feat setup: requirement tools for neovim's plugins

--- setup.sh
+++ setup.sh
@@ -140,16 +140,97 @@ function install {
 			sudo make install
 		}
 
+		function install_ripgrep {
+			if [[ "$(command -v rg)" ]]; then
+				return 0
+			fi
+
+			echo -e "Install ripgrep ..." >&1
+			$pkg_install_cmd ripgrep
+		}
+
+		function install_ranger {
+			if [[ $(command -v ranger) ]]; then
+				return 0
+			fi
+
+			echo -e "Install ranger ..." >&1
+			$pkg_install_cmd ranger
+
+		}
+
+		function install_htop {
+			if [[ $OSTYPE != linux-gnu* ]]; then
+				return
+			fi
+
+			if [[ "$(command -v htop)" ]]; then
+				return 0
+			fi
+
+			echo -e "Install htop ..." >&1
+			$pkg_install_cmd htop
+		}
+
+		function install_fzf {
+			if [[ "$(command -v fzf)" ]]; then
+				return 0
+			fi
+
+			echo -e "Install fzf ..." >&1
+			$pkg_install_cmd fzf
+		}
+
+		function install_ncdu {
+			if [[ $OSTYPE != linux-gnu* ]]; then
+				return 0
+			fi
+
+			if [[ "$(command -v ncdu)" ]]; then
+				return 0
+			fi
+
+			echo -e "Install ncdu ..." >&1
+			$pkg_install_cmd ncdu
+
+			return
+		}
+
+		function install_lazygit {
+			if [[ $(command -v lazygit) ]]; then
+				return 0
+			fi
+
+			echo -e "Install lazygit ..." >&1
+
+			if [[ $OSTYPE == linux-gnu* ]]; then
+				LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+				curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+				tar xf lazygit.tar.gz lazygit
+				sudo install lazygit /usr/local/bin
+			else
+				$pkg_install_cmd lazygit
+			fi
+
+			return
+		}
+
 		install_node
 		install_clangd
 		install_python_env
 		install_ctags
+		install_ripgrep
+		install_ranger
+		install_htop
+		install_fzf
+		install_ncdu
+		install_lazygit
 	}
 
 	local neovim_install_path='/tmp/neovim'
 	local neovim_config_path="$HOME/.config/nvim"
 
-	echo -e "\033[33mInstall Neovim\033[0m" >&1
+	echo -e "\033[33mSetup Neovim\033[0m" >&1
 
 	if [[ -z "$(command -v nvim)" ]]; then
 		install_build_prerequisites
@@ -163,7 +244,7 @@ function install {
 
 	relative_tools_install
 
-	echo -e "Success install neovim!" >&1
+	echo -e "Success setup neovim!" >&1
 }
 
 function link {
@@ -175,7 +256,7 @@ function main {
 	local sel="$1"
 
 	if [[ -z "$sel" ]]; then
-		read -p "Setup all required tools? [y/N] " -n 1 -r
+		read -p "Setup all required tools? [y/N] " -n 1 -e -r
 
 		if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 			sel="install"
