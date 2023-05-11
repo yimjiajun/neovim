150f9ed0b0d2a8116aa43a4d81e0b6aa946a8963
JiaJun Yim <yimjiajun@icloud.com>
2023-05-11 11:41:51 -0700

    rm autocmd: syntax display with whitespace trailing

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -28,16 +28,6 @@ vim.api.nvim_create_autocmd( "BufWritePre", {
 	end,
 })
 
-vim.api.nvim_create_autocmd( "Syntax", {
-	desc = "Display whitespace trailing",
-	group = "format",
-	pattern = "*",
-	callback = function()
-		vim.cmd([[match ExtraWhitespace /\s\+$\| \+\ze\t/]])
-		vim.cmd([[highlight ExtraWhitespace ctermbg=red guibg=red]])
-	end,
-})
-
 vim.api.nvim_create_augroup( "cursor", { clear = true })
 
 vim.api.nvim_create_autocmd( "InsertLeave, WinEnter", {
