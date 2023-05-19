13beda7b65f565377159fa186b95b656b4ec9d91
JiaJun Yim <v-yimjiajun@microsoft.com>
2023-05-19 13:05:13 -0700

    feat keymapiing: tags features

--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -12,7 +12,38 @@ require('telescope').setup{
 }
 
 local function setting_key_telescope()
-	vim.api.nvim_set_keymap('n', "z=", [[<cmd> lua require('telescope.builtin').spell_suggest() <CR>]] , { silent = true, desc = 'Spell Suggest' })
+	vim.api.nvim_set_keymap('n', "z=",
+		[[<cmd> lua require('telescope.builtin').spell_suggest() <CR>]],
+		{ silent = true, desc = 'Spell Suggest' })
+
+	if vim.fn.filereadable("tags") == 1 then
+		vim.api.nvim_set_keymap('n', "<leader>tt",
+			[[<cmd> lua require('telescope.builtin').current_buffer_tags() <CR>]],
+			{ silent = true, desc = 'buffer tags list' })
+	end
+
+	vim.api.nvim_set_keymap('n', "<leader>ts",
+		[[<cmd> lua require('telescope.builtin').tagstack() <CR>]],
+		{ silent = true, desc = 'tag stack selection' })
+
+	vim.api.nvim_set_keymap('n', "<leader>fb",
+		[[<cmd> lua require('telescope.builtin').current_buffer_fuzzy_find() <CR>]],
+		{ silent = true, desc = 'search current buffer' })
+
+	vim.api.nvim_set_keymap('n', "<leader>ft",
+		[[<cmd> lua require('telescope.builtin').tags() <CR>]],
+		{ silent = true, desc = 'search from tags' })
+
+	if pcall(require, "which-key") then
+		local wk = require("which-key")
+		wk.register({
+			t = { t = "tags list",
+				s = "tags stack" },
+			f = {
+				b = "buffer search",
+				t = "tags", },
+		}, { prefix = "<leader>" })
+	end
 end
 
 setting_key_telescope()
