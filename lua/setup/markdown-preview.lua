4938f908e2f67bbf68d8c7d57694fac23c2e5c8b
Yim JiaJun <v-yimjiajun@microsoft.com>
2023-06-09 11:28:35 -0700

    feat plugin/markdown: support to neovim builtin compiler
    keymapping: <leader> + b + <the number of MarkdowPreview>

--- lua/setup/markdown-preview.lua
+++ lua/setup/markdown-preview.lua
@@ -1,18 +1,55 @@
-vim.g.mkdp_filetypes = { "markdown" }
+local function setup_browser()
+	if vim.fn.has("mac") == 1 then
+		vim.g.mkdp_browser = 'safari'
+		return
+	end
 
-vim.g.browser = nil
-if vim.fn.has("mac") == 1 then
-	vim.g.mkdp_browser = 'safari'
-elseif vim.fn.has("win32") == 1 then
-	vim.g.mkdp_browser = 'MicrosoftEdge.exe'
-elseif vim.fn.has("unix") == 1 then
-	if vim.fn.isdirectory("/run/WSL") == 1 then
+	if vim.fn.has("win32") == 1 then
 		vim.g.mkdp_browser = 'MicrosoftEdge.exe'
-	else
+		return
+	end
+
+	if vim.fn.has("unix") == 1 then
+		if vim.fn.isdirectory("/run/WSL") == 1 then
+			vim.g.mkdp_browser = 'MicrosoftEdge.exe'
+			return
+		end
+
 		if vim.fn.executable("google-chrome") == 1 then
 			vim.g.mkdp_browser = 'google-chrome'
-		elseif vim.fn.executable("firefox") == 1 then
+			return
+		end
+
+		if vim.fn.executable("firefox") == 1 then
 			vim.g.mkdp_browser = 'firefox'
+			return
 		end
 	end
 end
+
+local function setup_builtin_compiler()
+	local md_compiler_data = {
+		name = "markdown preview on browser",
+		cmd = "MarkdownPreview",
+		desc = "preview current buffer markdown on browser",
+		ext = "markdown",
+		build_type = "builtin",
+		group = "plugin",
+	}
+
+	require('features.compiler').InsertInfo(
+		md_compiler_data.name,
+		md_compiler_data.cmd,
+		md_compiler_data.desc,
+		md_compiler_data.ext,
+		md_compiler_data.build_type,
+		md_compiler_data.group
+	)
+end
+
+vim.g.mkdp_filetypes = { "markdown" }
+vim.g.browser = nil
+setup_browser()
+setup_builtin_compiler()
+
+return nil
