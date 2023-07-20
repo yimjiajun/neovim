fb66d094e2f6bc46945988ed4a103042c5618a28
JiaJun Yim <yimjiajun@icloud.com>
2023-07-20 16:59:49 -0700

    feat plugin/fugitive: set local buffer status
    1. no relative number
    2. no number line
    3. no wrap in common git
    4. spelling in git commit

--- lua/setup/fugitive.lua
+++ lua/setup/fugitive.lua
@@ -1,8 +1,36 @@
-local function fugitve_setup_global_variable()
+local function fugitive_setup_global_variable()
 	vim.g.vim_git = "Git"
 end
 
-fugitve_setup_global_variable()
+local function fugitive_setup_autocmd()
+vim.api.nvim_create_augroup( "git", { clear = true })
+vim.api.nvim_create_autocmd( "FileType", {
+	desc = "Format git commit message",
+	group = "git",
+	pattern = "gitcommit",
+	callback = function()
+		vim.cmd("setlocal spell")
+		vim.cmd("setlocal wrap")
+		vim.cmd("setlocal expandtab")
+		vim.cmd("setlocal norelativenumber")
+		vim.cmd("setlocal nonumber")
+	end,
+})
+
+vim.api.nvim_create_autocmd( "FileType", {
+	desc = "Fromat common git",
+	group = "git",
+	pattern = "git",
+	callback = function()
+		vim.cmd("setlocal nowrap")
+		vim.cmd("setlocal norelativenumber")
+		vim.cmd("setlocal nonumber")
+	end,
+})
+end
+
+fugitive_setup_global_variable()
+fugitive_setup_autocmd()
 
 local ret = {}
 
