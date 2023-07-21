699759923cbf847458b77850f6e7f80ae2a42b3a
JiaJun Yim <yimjiajun@icloud.com>
2023-07-20 21:16:03 -0700

    feat install: gitui and dutree

--- install.sh
+++ install.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+display_center() {
+	local text="$1"
+	local text_width=${#text}
+	local screen_width="$(tput cols)"
+	local padding_width=$(( ($screen_width - $text_width) / 2 ))
+	printf "%${padding_width}s" " "
+	printf "%s\n" "$text"
+}
+
+display_title () {
+	local text="$1"
+	local screen_width="$(tput cols)"
+
+	for delimiter in {1..2}; do
+		for ((i=0; i<screen_width; i++)); do
+			echo -n "="
+		done
+
+		echo ""
+
+		echo -e -n "\e[1;33m"
+		if [ $delimiter -eq 1 ]; then
+			display_center "$text"
+		fi
+		echo -e -n "\e[0m"
+	done
+}
+
+
 function install_build_prerequisites {
 	echo -e "● Install build prerequisites..." >&1
 
@@ -49,7 +78,12 @@ function install_build_prerequisites {
 }
 
 function install_nvim {
-	local path="$1"
+
+	if [[ $(command -v nvim) ]]; then
+		return 0
+	fi
+
+	local path=$(mktemp -d)
 
 	echo -e "● Download neovim repository..." >&1
 	git clone --depth 1 https://github.com/neovim/neovim -b stable ${path} || {
@@ -156,7 +190,7 @@ function install_ctags {
 
 	git clone https://github.com/universal-ctags/ctags.git $path 1>/dev/null || {
 		echo -e "\033[31mError: git clone ctags failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 
 	cd $path
@@ -164,25 +198,25 @@ function install_ctags {
 	echo -e "● ctags auto generation ..." >&1
 	./autogen.sh 1>/dev/null || {
 		echo -e "\033[31mError: autogen ctags failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 
 	echo -e "● ctags configure ..." >&1
 	./configure --prefix="$install_path" 1>/dev/null || {
 		echo -e "\033[31mError: configure ctags failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 
 	echo -e "● ctags make ..." >&1
 	make 1>/dev/null 2>&1 || {
 		echo -e "\033[31mError: make ctags failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 
 	echo -e "● ctags make install ..." >&1
 	sudo make install 1>/dev/null || {
 		echo -e "\033[31mError: make install ctags failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 }
 
@@ -194,7 +228,7 @@ function install_ripgrep {
 	echo -e "● Install ripgrep ..." >&1
 	$pkg_install_cmd ripgrep 1>/dev/null || {
 		echo -e "\033[31mError: Install ripgrep failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 }
 
@@ -206,7 +240,7 @@ function install_ranger {
 	echo -e "● Install ranger ..." >&1
 	$pkg_install_cmd ranger 1>/dev/null || {
 		echo -e "\033[31mError: Install ranger failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 }
 
@@ -222,7 +256,7 @@ function install_htop {
 	echo -e "● Install htop ..." >&1
 	$pkg_install_cmd htop 1>/dev/null || {
 		echo -e "\033[31mError: Install htop failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 }
 
@@ -234,7 +268,7 @@ function install_fzf {
 	echo -e "● Install fzf ..." >&1
 	$pkg_install_cmd fzf 1>/dev/null || {
 		echo -e "\033[31mError: Install fzf failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 }
 
@@ -250,7 +284,7 @@ function install_ncdu {
 	echo -e "● Install ncdu ..." >&1
 	$pkg_install_cmd ncdu 1>/dev/null || {
 		echo -e "\033[31mError: Install ncdu failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 }
 
@@ -269,26 +303,26 @@ function install_lazygit {
 		LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 		curl -Lo $tmp_path/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" 1>/dev/null || {
 			echo -e "\033[31mError: Download lazygit failed!\033[0m" >&2
-			exit 1
+			return 1
 		}
 
 		echo -e "● Extract lazygit ..." >&1
 
 		tar xf $tmp_path/lazygit.tar.gz -C $tmp_path || {
 			echo -e "\033[31mError: Extract lazygit failed!\033[0m" >&2
-			exit 1
+			return 1
 		}
 
 		echo -e "● Install lazygit ..." >&1
 
 		sudo install $tmp_path/lazygit /usr/local/bin || {
 			echo -e "\033[31mError: Install lazygit failed!\033[0m" >&2
-			exit 1
+			return 1
 		}
 	else
 		$pkg_install_cmd lazygit 1>/dev/null || {
 			echo -e "\033[31mError: Install lazygit failed!\033[0m" >&2
-			exit 1
+			return 1
 		}
 	fi
 }
@@ -305,7 +339,7 @@ function install_khal {
 	echo -e "● Install khal ..." >&1
 	$pkg_install_cmd khal 1>/dev/null || {
 		echo -e "\033[31mError: Install khal failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 }
 
@@ -321,66 +355,141 @@ function install_bpytop {
 	echo -e "● Install bpytop ..." >&1
 	pip3 install --upgrade-strategy eager bpytop 1>/dev/null || {
 		echo -e "\033[31mError: Install bpytop failed!\033[0m" >&2
-		exit 1
+		return 1
 	}
 }
 
-display_center() {
-	local text="$1"
-	local text_width=${#text}
-	local screen_width="$(tput cols)"
-	local padding_width=$(( ($screen_width - $text_width) / 2 ))
-	printf "%${padding_width}s" " "
-	printf "%s\n" "$text"
+function install_cargo {
+
+	if [[ $(command -v rustup) ]]; then
+		return 0
+	fi
+
+	display_center "Install cargo"
+
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y || {
+		$common display_error "install $tool failed !"
+		return 1
+	}
+
+	if [[ -f "$HOME/.cargo/env" ]]; then
+		if [[ -f "$HOME/.$(basename $SHELL)rc" ]] && \
+			[[ $(grep -c "source $HOME/.cargo/env" "$HOME/.$(basename $SHELL)rc") -eq 0 ]]; \
+		then
+			echo "source $HOME/.cargo/env" >> $HOME/.$(basename $SHELL)rc
+		fi
+
+		source "$HOME/.cargo/env" || {
+			$common display_error "source $HOME/.cargo/env failed !"
+			return 1
+		}
+
+		rustup default stable || {
+			$common display_error "install stable failed !"
+			return 1
+		}
+	else
+		$common display_error ".cargo/eve not found !"
+		return 1
+	fi
 }
 
-display_title () {
-	local text="$1"
-	local screen_width="$(tput cols)"
+function install_dutree {
 
-	for delimiter in {1..2}; do
-		for ((i=0; i<$screen_width; i++)); do
-			echo -n "="
-		done
+	if [[ $(command -v dutree) ]]; then
+		return 0
+	fi
 
-		echo ""
+	display_center "Install dutree"
 
-		echo -e -n "\e[1;33m"
-		if [ $delimiter -eq 1 ]; then
-			display_center "$text"
+	if [[ -z $(command -v cargo) ]]; then
+		echo -e "\e[33mWarning: skip to install dutree ... cargo nout found\e[0m" >&2
+		return 0
+	fi
+
+	cargo install dutree 1>/dev/null || {
+		$common display_error "install dutree failed !"
+		return 1
+	}
+
+	return 0
+}
+
+function install_gitui {
+
+	if [[ $(command -v gitui) ]]; then
+		return 0
+	fi
+
+	display_center "Install gitui"
+
+	local arch=$(uname -m)
+	local pkg=nil
+	local ver='v0.23.0'
+
+	if [[ $OSTYPE == darwin* ]]; then
+		pkg='gitui-mac.tar.gz'
+	elif [[ $OSTYPE == linux-gnu* ]]; then
+		if [[ $arch == 'x86_64' ]]; then
+			pkg='gitui-linux-musl.tar.gz'
+		elif [[ $arch == 'aarch64' ]]; then
+			pkg='gitui-linux-aarch64.tar.gz'
+		else
+			echo -e "\e[33mWarning: skip to install gitui ... arch $arch not supported\e[0m" >&2
+			return 0
 		fi
-		echo -e -n "\e[0m"
-	done
+	else
+		echo -e "\e[33mWarning: skip to install gitui ... os $OSTYPE not supported\e[0m" >&2
+		return 0
+	fi
+
+	local tmp_path=$(mktemp -d)
+
+	curl -Lo $tmp_path/$pkg "https://github.com/extrawurst/gitui/releases/download/${ver}/${pkg}" || {
+		$common display_error "download gitui failed !"
+		return 1
+	}
+
+	tar -zxf $tmp_path/$pkg -C $HOME/.local/bin/ || {
+		$common display_error "install gitui failed !"
+		return 1
+	}
+
+	return 0
 }
 
 function main {
-	local neovim_install_path="$(mktemp -d)"
-	local neovim_config_path="$HOME/.config/nvim"
+	local install_failed=0
+	local failed_pkgs=()
+	local intsall_pkgs=(\
+		'install_build_prerequisites' \
+		'install_nvim' \
+		'install_node'		'install_clangd'	'install_python' \
+		'install_ctags'		'install_ripgrep'	'install_ranger' \
+		'install_htop'		'install_fzf'		'install_ncdu' \
+		'install_lazygit'	'install_khal'		'install_bpytop' \
+		'install_cargo'		'install_dutree'	'install_gitui' \
+	)
 
-	if [[ -z "$(command -v nvim)" ]]; then
-		install_build_prerequisites
+	for func in ${intsall_pkgs[@]}; do
+		display_title "$func"
+		$func || {
+			display_center '\e[31minstall $func failed !\e[0m'
+			failed_pkgs+=($func)
+			install_failed=1
+		}
 
-		if [[ -d "$neovim_install_path" ]]; then
-			rm -rf $neovim_install_path
-		fi
-
-		install_nvim $neovim_install_path
-	fi
-
-	install_node
-	install_clangd
-	install_python
-	install_ctags
-	install_ripgrep
-	install_ranger
-	install_htop
-	install_fzf
-	install_ncdu
-	install_lazygit
-	install_khal
-	install_bpytop
+	done
 
 	$SHELL -c "source ${HOME}/.$(basename $SHELL)rc"
+
+	if [[ $install_failed -eq 1 ]]; then
+		echo -e "\e[31mError: install failed !\e[0m" >&2
+		echo -e "\e[31mFailed packages: $(sed 's/\w*_//g' <<< ${failed_pkgs[@]})\e[0m" >&2
+		return 1
+	fi
+
+	return 0
 }
 
 display_title "Setup Neovim"
@@ -412,7 +521,11 @@ case "$OSTYPE" in
 		;;
 esac
 
-main "$@"
+main "$@" || {
+	display_center "\e[31mSetup Neovim failed !\e[0m"
+	exit 1
+}
+
 display_title "Success Setup Neovim!"
 
 exit 0
