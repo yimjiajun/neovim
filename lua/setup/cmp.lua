927eb2e99080e110901710f1b49867fbc530231c
JiaJun Yim <yimjiajun@icloud.com>
2023-08-16 22:50:43 -0700

    refactor cmp: change select next items key
    <C-n>: next item
    <C-p>: previous item
    
    default <tab> will conflict with copilot accept key

--- lua/setup/cmp.lua
+++ lua/setup/cmp.lua
@@ -30,7 +30,7 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ['<C-n>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -39,7 +39,7 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ['<C-p>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
