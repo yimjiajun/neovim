4ad10b5fd4a58ed5367b2b68f9c6ed858c92736b
JiaJun Yim <yimjiajun@icloud.com>
2023-10-09 19:36:56 -0700

    feat plugin/csv: autocmd only support seperate event at nvim v.9.4.0

--- lua/features/plugin.lua
+++ lua/features/plugin.lua
@@ -227,22 +227,33 @@ local function setup()
 			},
 			{ 'chrisbra/csv.vim',
 				config = function ()
-					vim.api.nvim_create_autocmd( "BufRead,BufNewFile", {
-						desc = "csv format display",
-						group = "format",
-						pattern = "{*.csv,*.dat}",
-						callback = function()
-							local did_load_csvfiletype
-
+					function _G.load_csv_filetype()
 							if vim.fn.exists('did_load_csvfiletype') then
 								return
 							end
 
-							did_load_csvfiletype = 1
+							local did_load_csvfiletype = 1
 
 							if did_load_csvfiletype == 1 then
 								vim.bo.filetype = 'csv'
 							end
+					end
+
+					vim.api.nvim_create_autocmd("BufRead", {
+						desc = "csv format display",
+						group = "format",
+						pattern = "{*.csv,*.dat}",
+						callback = function()
+							_G.load_csv_filetype()
+						end,
+					})
+
+					vim.api.nvim_create_autocmd("BufNewFile", {
+						desc = "csv format display",
+						group = "format",
+						pattern = "{*.csv,*.dat}",
+						callback = function()
+							_G.load_csv_filetype()
 						end,
 					})
 				end,
