b70cc4905080587d12ebcce9dbbed50b7d578685
JiaJun Yim <yimjiajun@icloud.com>
2023-10-17 22:50:35 -0700

    feat package: support native vim package

--- lua/features/plugin.lua
+++ lua/features/plugin.lua
@@ -1,5 +1,25 @@
+local function native_package_install()
+	local packages = {}
+	local d = vim.fn.has('win32') ~= 0 and "\\" or "/"
+	local path = vim.fn.stdpath('data') .. d .. 'site' .. d .. 'pack' .. d .. 'jun' .. d .. 'start'
+
+	if vim.fn.isdirectory(path) == 0 then
+		vim.api.nvim_echo({ { "package: create package directory ... " .. path } }, false, {})
+		vim.fn.mkdir(path, "p")
+	end
+
+	for i, v in ipairs(packages) do
+		local pack_dir = path .. d .. v.name
+		if vim.fn.isdirectory(pack_dir) == 0 then
+			vim.api.nvim_echo({ { "package: download " .. v.name } }, false, {})
+			vim.fn.system("git clone " .. v.url .. " " .. pack_dir)
+		end
+	end
+end
+
 local function setup()
 	if vim.g.enable_plugin == 0 then
+		native_package_install()
 		return
 	end
 
