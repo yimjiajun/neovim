24e8cc73955e771c0c5236d9451d8d7fc6e74464
JiaJun Yim <yimjiajun@icloud.com>
2023-10-21 00:36:32 -0700

    feat plugin: telescope upgrade to v0.1.4 to compatible neovim v9.4.0

--- lua/features/plugin.lua
+++ lua/features/plugin.lua
@@ -92,7 +92,7 @@ local function setup()
 				end
 			},
 			{	'nvim-telescope/telescope.nvim',
-				tag = '0.1.1',
+				tag = '0.1.4',
 				dependencies = { 'nvim-lua/plenary.nvim' },
 				config = function()
 					require('plugin.telescope').setup()
