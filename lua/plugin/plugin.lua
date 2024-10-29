78516c81cdd455647a6b984df8911f8619ce20af
JiaJun Yim <yimjiajun@icloud.com>
2024-10-29 23:58:33 +0800

    feat plugin: add gitgraph.nvim with diffview integration
    - Added `isakbm/gitgraph.nvim` plugin with `sindrets/diffview.nvim` as a
      dependency.
    - Configured symbols for merge commits and regular commits.
    - Set up custom timestamp format and fields for gitgraph.
    - Implemented hooks for selecting commits and commit ranges to open diffs
      using Diffview.
    - Added keybinding for drawing the git graph with a maximum count of 5000.

--- lua/plugin/plugin.lua
+++ lua/plugin/plugin.lua
@@ -229,7 +229,48 @@ local function setup()
                 config = function()
                     require('render-markdown').setup({})
                 end
-            }
+            }, {
+                'isakbm/gitgraph.nvim',
+                dependencies = { "sindrets/diffview.nvim" },
+                opts = {
+                    symbols = {
+                        merge_commit = '●',
+                        commit = '○',
+                        merge_commit_end = '✗',
+                        commit_end = '┳'
+                    },
+                    format = {
+                        timestamp = '%H:%M:%S %d-%m-%Y',
+                        fields = {
+                            'hash', 'timestamp', 'author', 'branch_name', 'tag'
+                        }
+                    },
+                    hooks = {
+                        -- Check diff of a commit
+                        on_select_commit = function(commit)
+                            vim.notify('DiffviewOpen ' .. commit.hash .. '^!')
+                            vim.cmd(':DiffviewOpen ' .. commit.hash .. '^!')
+                        end,
+                        -- Check diff from commit a -> commit b
+                        on_select_range_commit = function(from, to)
+                            vim.notify('DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
+                            vim.cmd(':DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
+                        end
+                    }
+                },
+                keys = {
+                    {
+                        "<leader>ggl",
+                        function()
+                            require('gitgraph').draw({}, {
+                                all = true,
+                                max_count = 5000
+                            })
+                        end,
+                        desc = "GitGraph - Draw"
+                    }
+                }
+            }, { "sindrets/diffview.nvim" }
         },
         defaults = {
             -- By default, Your custom plugins will load during startup.
