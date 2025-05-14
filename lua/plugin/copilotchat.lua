30e3df3f1f2dbaad6577d334ea1e8a86816dcb62
Richard Yim <richardyim@ami.com>
2025-05-14 10:07:31 +0800

    lua: plugin: copilotchat: enhance configuration and keybindings
    - Introduced `prefix_key` for consistent keybinding prefixes.
    - Added `config` table to customize CopilotChat settings.
    - Integrated new prompts: `CommitStaged` and `ImproveGrammar`.
    - Updated window layout options for better customization.
    - Replaced deprecated `CopilotChat.integrations.telescope` usage.
    - Removed redundant keybinding for `CopilotChatCommit`.

--- lua/plugin/copilotchat.lua
+++ lua/plugin/copilotchat.lua
@@ -1,23 +1,61 @@
+local prefix_key = '<leader>gc'
+local function get_config()
+    print('get_config')
+end
+local config =  {
+    model = "gpt-4", -- :CoplotChatModels
+    prompts = {
+        CommitStaged = {
+            prompt = [[Write commit message for the change with commitizen convention.]] ..
+                [[Make sure the title has maximum 50 characters and message is wrapped at 72 characters.]] ..
+                [[Wrap the whole message in code block with language gitcommit.]],
+            system_prompt = 'Commitizen is a tool that helps you write better commit messages',
+            mapping = prefix_key .. 's',
+            description = 'Commit staged changes',
+            context = 'git:staged',
+        },
+        ImproveGrammer = {
+            prompt = vim.fn.getreg('*') .. [[Improve above english grammar without display prompt.]],
+            system_prompt = 'You are a helpful assistant that improves English grammar',
+            mapping = prefix_key .. 'I',
+            description = 'Improve English Grammer (clipboard)'
+        },
+    },
+    highlight_selection = false, -- Highlight selection
+    window = {
+        layout = 'float', -- 'vertical', 'horizontal', 'float', 'replace'
+        width = 0.9, -- fractional width of parent, or absolute width in columns when > 1
+        height = 0.7, -- fractional height of parent, or absolute height in rows when > 1
+        -- Options below only apply to floating windows
+        relative = 'editor', -- 'editor', 'win', 'cursor', 'mouse'
+        border = 'rounded', -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+        row = nil, -- row position of the window, default is centered
+        col = nil, -- column position of the window, default is centered
+        title = 'Copilot Chat', -- title of chat window
+        footer = nil, -- footer of chat window
+        zindex = 1, -- determines if window is on top or below other floating windows
+    },
+}
 local function setup()
-    -- Setup CopilotChat
-    require('CopilotChat').setup()
     -- Setup keybindings
     local keymap = vim.keymap.set
     local keymap_options = function(desc)
         return { noremap = true, silent = true, desc = desc }
     end
-    local key = '<leader>gc'
     local copilotchat_telescope = function()
         local actions = require("CopilotChat.actions")
         require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
     end
     for _, mode in ipairs({ 'n', 'v' }) do
-        keymap(mode, key, copilotchat_telescope, keymap_options('Open CopilotChat'))
+        keymap(mode, prefix_key, copilotchat_telescope, keymap_options('Open CopilotChat'))
     end
+
+    require('CopilotChat').setup(config)
     -- Setup which-key mappings
     if pcall(require, 'which-key') then
         local wk = require('which-key')
         local keys = {
+            -- default CopilotChat commands
             { "<leader>gc", group = "CopilotChat" },
             { "<leader>gcC", ":CopilotChatToggle<CR>", desc = "Open" },
             { "<leader>gcd", ":CopilotChatDocs<CR>", desc = "Docs" },
@@ -25,7 +63,6 @@ local function setup()
             { "<leader>gcf", ":CopilotChatFix<CR>", desc = "Fix" },
             { "<leader>gco", ":CopilotChatOptimize<CR>", desc = "Optimize" },
             { "<leader>gcr", ":CopilotChatReview<CR>", desc = "Review" },
-            { "<leader>gcs", ":CopilotChatCommit<CR>", desc = "Commit" },
             { "<leader>gct", ":CopilotChatTests<CR>", desc = "Tests" }
         }
         wk.add({ mode = { 'n' }, keys })
