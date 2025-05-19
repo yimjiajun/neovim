ef738216c6ad8c6215bd51d813243e98f751e282
JiaJun Yim <yimjiajun@icloud.com>
2025-05-20 02:17:55 +0800

    plugin: neovide: add scale and fullscreen keybindings
    Added functionality to adjust the Neovide scale factor with keybindings
    and toggle fullscreen mode. Introduced a helper function to manage scale
    adjustments. Integrated `which-key` for better keybinding discovery.
    
    - Added keybindings for increasing, decreasing, and resetting scale.
    - Added keybinding to toggle fullscreen mode.
    - Added keybinding to toggle command line animation.
    - Integrated `which-key` for Neovide keybinding group.

--- lua/plugin/neovide.lua
+++ lua/plugin/neovide.lua
@@ -2,7 +2,26 @@ if vim.g.neovide == nil then
     return {}
 end
 
+local adjust_scale_factor = function(value)
+    if value == 0 then
+        vim.g.neovide_scale_factor = 1.0
+        return
+    end
+
+    if vim.g.neovide_scale_factor + value < 0.1 then
+        return
+    end
+
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + value
+end
+
 local function setup()
+    local prefix_key = '<leader>gn'
+    local keymap = vim.keymap.set
+    local keymap_options = function(desc)
+        return { noremap = true, silent = true, desc = desc }
+    end
+
     vim.g.neovide_scale_factor = 1.0
     vim.g.neovide_scroll_animation_length = 1.0
     vim.g.neovide_hide_mouse_when_typing = true
@@ -20,6 +39,43 @@ local function setup()
 
     vim.opt.title = true
     vim.opt.titlestring = "JunVim" .. "@" .. vim.fn.getcwd()
+
+    for _, mode in ipairs({ 'n', 'v' }) do
+        keymap(mode, prefix_key .. '+', function()
+            adjust_scale_factor(0.2)
+        end, keymap_options('Increase scale'))
+
+        keymap(mode, prefix_key .. '-', function()
+            adjust_scale_factor(-0.2)
+        end, keymap_options('Decrease scale'))
+
+        keymap(mode, prefix_key .. '=', function()
+            adjust_scale_factor(0.0)
+        end, keymap_options('Restore scale'))
+
+        keymap(mode, prefix_key .. 'f', function()
+            if (vim.g.neovide_fullscreen == true) then
+                vim.g.neovide_fullscreen = false
+            else
+                vim.g.neovide_fullscreen = true
+            end
+        end, keymap_options('Toggle fullscreen'))
+
+        keymap(mode, prefix_key .. 'c', function()
+            if (vim.g.neovide_cursor_animate_command_line == true) then
+                vim.g.neovide_cursor_animate_command_line = false
+            else
+                vim.g.neovide_cursor_animate_command_line = true
+            end
+        end, keymap_options('Toggle command line anime'))
+    end
+
+    if pcall(require, 'which-key') then
+        local wk = require('which-key')
+        wk.add({
+            { prefix_key , group = "Neovide" },
+        })
+    end
 end
 
 return { Setup = setup }
